a81633e02a5637205f588b797fe0d615ccd8bb36
Oleksandr Sakun <sakun.olesk@gmail.com>
2019-08-27 17:40:23 +0300

    :green_heart: Fix

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Tournament app is running!');
+    expect(page.getTitleText()).toEqual('Hello from angular');
   });
 
   afterEach(async () => {
