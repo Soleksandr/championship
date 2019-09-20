87fa00d14447253ecda55d57c3b988f43dadf295
Oleksandr Sakun <sakun.olesk@gmail.com>
2019-09-20 23:30:03 +0300

    :white_check_mark: Add tests for Sign Up form

--- src/app/forms/sign-up/sign-up.component.spec.ts
+++ src/app/forms/sign-up/sign-up.component.spec.ts
@@ -33,11 +33,11 @@ describe('SignUpComponent', () => {
     .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     fixture = TestBed.createComponent(SignUpComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
@@ -52,4 +52,54 @@ describe('SignUpComponent', () => {
     const h1 = fixture.debugElement.query(By.css('h1')).nativeElement;
     expect(h1.textContent).toContain('Sign Up');
   });
+
+  it('should not display validation errors initially', () => {
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.query(By.css('#email-validate')).nativeElement.hidden).toEqual(true);
+    expect(fixture.debugElement.query(By.css('#password-validate')).nativeElement.hidden).toEqual(true);
+    expect(fixture.debugElement.query(By.css('#confirm_password-validate')).nativeElement.hidden).toEqual(true);
+  })
+
+  it('should be rendered with disabled submit button', async(() => {
+    fixture.detectChanges();
+
+    const button = fixture.debugElement.query(By.css('[type="submit"')).nativeElement
+
+    expect(button.disabled).toEqual(true)
+  }))
+
+  it('should validate empty fields', async(() => {
+    const inputEmail = fixture.debugElement.query(By.css('[name="email"]')).nativeElement;
+    const inputPassword = fixture.debugElement.query(By.css('[name="password"]')).nativeElement;
+    const inputConfirmPassword = fixture.debugElement.query(By.css('[name="confirmPassword"]')).nativeElement;
+
+    inputEmail.dispatchEvent(new Event('blur'));
+    inputPassword.dispatchEvent(new Event('blur'));
+    inputConfirmPassword.dispatchEvent(new Event('blur'));
+
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.query(By.css('#email-validate')).nativeElement.hidden).toEqual(false);
+    expect(fixture.debugElement.query(By.css('#password-validate')).nativeElement.hidden).toEqual(false);
+    expect(fixture.debugElement.query(By.css('#confirm_password-validate')).nativeElement.hidden).toEqual(false);
+  }));
+
+  it('should display proper validation error for inequality password', async(() => {
+    const inputPassword = fixture.debugElement.query(By.css('[name="password"]')).nativeElement;
+    const inputConfirmPassword = fixture.debugElement.query(By.css('[name="confirmPassword"]')).nativeElement;
+
+    inputPassword.value = 'password'
+    inputConfirmPassword.value = 'another'
+
+    inputPassword.dispatchEvent(new Event('input'));
+    inputConfirmPassword.dispatchEvent(new Event('input'));
+    inputConfirmPassword.dispatchEvent(new Event('blur'));
+
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.query(By.css('#confirm_password-validate')).nativeElement.innerText).toEqual('Confirm password must be the same as password');
+  }));
 });
+
+// TODO add tests for submit
