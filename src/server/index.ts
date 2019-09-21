1e1215344911fa82999d16681a6def5f8b920338
Oleksandr Sakun <sakun.olesk@gmail.com>
2019-09-22 00:31:15 +0300

    :sparkles: Server basic implementation

---
+++ src/server/index.ts
@@ -0,0 +1,22 @@
+import Koa from 'koa';
+
+const {
+  PORT = 4444,
+  NODE_ENV = 'development'
+} = process.env;
+
+const app = new Koa();
+
+app.env = NODE_ENV;
+
+app.use(async ctx => {
+  ctx.body = 'Koa is running';
+});
+
+app.on('error', (err) => {
+  console.error('- Error - ', err);
+});
+
+app.listen(PORT);
+
+console.log(`Server is running on ${PORT} port in ${app.env} mode`);
