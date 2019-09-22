46fade3457861f706e9a49ac0287ac7286fc9daa
Oleksandr Sakun <sakun.olesk@gmail.com>
2019-09-23 00:50:44 +0300

    :bug: Fix unit test

--- src/app/services/http-request/http-request.service.spec.ts
+++ src/app/services/http-request/http-request.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { HttpRequestService } from './http-request.service';
 
 describe('HttpRequestService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule]
+  }));
 
   it('should be created', () => {
     const service: HttpRequestService = TestBed.get(HttpRequestService);
