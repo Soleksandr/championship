d483506029a79ce82b63654af9462cf9930d86a0
Oleksandr Sakun <sakun.olesk@gmail.com>
2019-09-27 23:41:14 +0300

    :speech_balloon: Update project name

--- packages/client/src/app/app.component.spec.ts
+++ packages/client/src/app/app.component.spec.ts
@@ -41,16 +41,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Tournament'`, () => {
+  it(`should have as title 'Championship'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Tournament');
+    expect(app.title).toEqual('Championship');
   });
 
   // it('should render title', () => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   fixture.detectChanges();
   //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('Tournament app is running!');
+  //   expect(compiled.querySelector('.content span').textContent).toContain('championship app is running!');
   // });
 });
