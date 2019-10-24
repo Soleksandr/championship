aa02bc65c3aa363f9123220f3d39e64c2d4c62f6
Oleksandr Sakun <sakun.olesk@gmail.com>
2019-10-25 01:13:37 +0300

    :construction: Uncommit db

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,15 +1,12 @@
 import app from './app';
-// import db from './db';
+import db from './db';
 
 const { PORT = 4444 } = process.env;
 
-app.listen(PORT);
+db.init()
+  .then(() => {
+    app.listen(PORT);
 
-console.log(`Server is running on ${PORT} port in ${app.env} mode`);
-// db.init()
-//   .then(() => {
-//     app.listen(PORT);
-
-//     console.log(`Server is running on ${PORT} port in ${app.env} mode`);
-//   })
-//   .catch(err => console.error(err));
+    console.log(`Server is running on ${PORT} port in ${app.env} mode`);
+  })
+  .catch(err => console.error(err));
